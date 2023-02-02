58e37713227f2d9d299d589ca0c7651d96bf33a6
SlashLight <savvateev_andreyka@mail.ru>
2023-02-02 17:39:19 +0300

    added mux

--- reddit/redditclone/cmd/redditclone/main.go
+++ reddit/redditclone/cmd/redditclone/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import "github.com/gorilla/mux"
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/register", userHandler.Register).Methods("POST")
+	r.HandleFunc("/api/login", userHandler.Login).Methods("POST")
+
+	r.HandleFunc("/api/posts/{category}", handlers.ListByCategory).Methods("GET")
+	r.HandleFunc("/api/posts/", handlers.List).Methods("GET")
+	r.HandleFunc("/api/posts/", handlers.Add).Methods("POST")
+	r.HandleFunc("/api/post/{post_id}", handlers.PostById).Methods("GET")
+	r.HandleFunc("/api/post/{post_id}", handlers.AddComment).Methods("POST")
+	r.HandleFunc("/api/post/{post_id}/{comment_id}", handlers.DeleteComment).Methods("DELETE")
+	r.HandleFunc("/api/post/{post_id}/upvote", handlers.Upvote).Methods("GET")
+	r.HandleFunc("/api/post/{post_id}/downvote", handlers.Downvote).Methods("GET")
+	r.HandleFunc("/api/post/{post_id}", handlers.DeletePost).Methods("DELETE")
+	r.HandleFunc("/api/user/{user_id}", handlers.ListByAuthor).Methods("GET")
+}
--- reddit/redditclone/go.mod
+++ reddit/redditclone/go.mod
@@ -3,6 +3,7 @@ module reddit
 go 1.19
 
 require (
+	github.com/gorilla/mux v1.8.0
 	go.uber.org/zap v1.24.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
